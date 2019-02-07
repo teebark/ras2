8344f87e518614098bb5a67c1e21f200265a3884
teebark <terry@teebark.com>
2019-02-07 11:52:59 -0500

    moved from ras2

--- functions.php
+++ functions.php
@@ -1,7 +1,22 @@
 <?php
-/* Disable secondary navigation on mobile */
-add_action( 'wp_enqueue_scripts', 'generate_dequeue_secondary_nav_mobile', 999 );
-function generate_dequeue_secondary_nav_mobile() {
-    wp_dequeue_style( 'generate-secondary-nav-mobile' );
+// Add External Link to Featured Image with Custom Field
+add_filter('post_thumbnail_html','add_external_link_on_page_post_thumbnail',10);
+    function add_external_link_on_page_post_thumbnail( $html ) {
+    if( is_singular() ) {
+            global $post;
+			$src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size='full', false );
+			$src = get_the_post_thumbnail_url($post,'full');
+            $name = get_post_meta($post->ID, 'ExternalUrl', true);
+            if( $name ) {
+                    $html = '<a href="' . ( $src ) . '" target="_blank" >' . $html . '</a>';
+            }
+    }
+    return $html;
+    }
+// Make image size on post page = medium large
+add_filter( 'generate_page_header_default_size', 'generate_custom_default_page_header_size' );
+function generate_custom_default_page_header_size()
+{
+      return 'medium_large';
 }
 ?>
