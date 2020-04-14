fc09dc2608dbdcd5599a1c7e24442fd46bfd7877
Terry Barker <terry@teebark.com>
2020-04-14 14:00:55 -0400

    responsive tables

--- functions.php
+++ functions.php
@@ -1,22 +1,11 @@
-<?php
-// Add External Link to Featured Image with Custom Field
-add_filter('post_thumbnail_html','add_external_link_on_page_post_thumbnail',10);
-    function add_external_link_on_page_post_thumbnail( $html ) {
-    if( is_singular() ) {
-            global $post;
-			$src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size='full', false );
-			$src = get_the_post_thumbnail_url($post,'full');
-            $name = get_post_meta($post->ID, 'ExternalUrl', true);
-            if( $name ) {
-                    $html = '<a href="' . ( $src ) . '" target="_blank" >' . $html . '</a>';
-            }
-    }
-    return $html;
-    }
-// Make image size on post page = medium large
-add_filter( 'generate_page_header_default_size', 'generate_custom_default_page_header_size' );
-function generate_custom_default_page_header_size()
-{
-      return 'medium_large';
+<?php 
+// JS for responsive tables
+/**
+ * Enqueue a script
+ */
+function myprefix_enqueue_scripts() {
+	wp_register_script('table-resp', get_stylesheet_directory_uri() . '/js/table.js', array() );
+    wp_enqueue_script( 'table-resp', get_stylesheet_directory_uri() . '/js/table.js', array(), true );
 }
+add_action( 'wp_enqueue_scripts', 'myprefix_enqueue_scripts' );
 ?>
